b5ff762d3e78e7fba84f2397d41bb1ccd5aaf3f2
ChandanSayed <hangzhou635@gmail.com>
2024-10-09 18:31:50 +0600

    Sign Up

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,7 +1,41 @@
 <script setup>
+const router = useRouter();
+
 let email = ref("");
 let password1 = ref("");
 let password2 = ref("");
+let errors = ref([]);
+
+async function submitForm() {
+  console.log("submitForm");
+
+  errors.value = [];
+
+  await $fetch("http://127.0.0.1:8000/api/users/", {
+    method: "POST",
+    body: {
+      username: email.value,
+      password: password1.value,
+    },
+  })
+    .then((response) => {
+      console.log("response", response);
+
+      router.push({ path: "/login" });
+    })
+    .catch((error) => {
+      if (error.response) {
+        for (const property in error.response._data) {
+          errors.value.push(`${property}: ${error.response._data[property]}`);
+        }
+        console.log(JSON.stringify(error.response));
+      } else if (error.message) {
+        errors.value.push("Something went wrong. Please try again");
+
+        console.log(JSON.stringify(error));
+      }
+    });
+}
 </script>
 
 <template>
@@ -28,6 +62,15 @@ let password2 = ref("");
           placeholder="Repeat password..."
           class="w-full mb-4 py-4 px-6 rounded-xl"
         />
+
+        <div
+          v-if="errors.length"
+          class="mb-6 py-4 px-6 bg-rose-400 text-white rounded-xl"
+        >
+          <p v-for="error in errors" v-bind:key="error">
+            {{ error }}
+          </p>
+        </div>
 
         <button class="py-4 px-6 bg-teal-700 text-white rounded-xl">
           Submit
